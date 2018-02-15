0c22ae054b2e17d3df28c52d83e1270d9c979982
Frank Li <frankli0324@hotmail.com>
2018-02-15 19:47:56 +0800

    happy new year

--- SocketWrapper.h
+++ SocketWrapper.h
@@ -14,14 +14,11 @@ using namespace std;
 //#define failed 1
 //#define success 0
 
-enum SocketErrorCode {
-	success,
-	init_failed,//please check your internet settings
-	connect_failed//please check your internet connection
-};
 enum SocketStatusCode {
 	initialized, init_failed,
-	connected, connect_failed
+	connected, connect_failed,
+	msg_sent, send_failed,
+	runtime_error
 };
 class Socket {
 	char buffer[1024];
@@ -37,13 +34,25 @@ class Socket {
 		return SocketStatusCode::initialized;
 	}
 public:
-	Socket() { 
-		try_to_do((status = initialize())==SocketStatusCode::init_failed);
+	Socket() {
+		try_to_do((status = initialize()) == SocketStatusCode::init_failed);
 	}
 	~Socket() { closesocket(sock); }
 
+
+	//server
+	int perform_listen(int max_connection) {
+		int error_flag = 0, conn_handler;
+		try_to_do((error_flag = listen(sock, max_connection)) == SOCKET_ERROR);
+		if (error_flag == SOCKET_ERROR)return SocketStatusCode::runtime_error;
+		while (1) {
+			try_to_do((error_flag = conn_handler = accept(sock, (sockaddr*)NULL, NULL)) == SOCKET_ERROR);
+			if (error_flag == SOCKET_ERROR)return SocketStatusCode::runtime_error;
+		}
+	}
+	//client
 	int perform_connect(int port) {
-		int error_flag=SOCKET_ERROR;
+		int error_flag = SOCKET_ERROR;
 		sockaddr_in address = { AF_INET };
 
 		try_to_do((error_flag = bind(sock, (sockaddr*)&address, sizeof(address))) == SOCKET_ERROR);
@@ -61,8 +70,13 @@ public:
 
 		return SocketStatusCode::connected;
 	}
-	int perform_send(string request, string host) {
-		
+
+	int perform_send(string request) {
+		int fflag = 0;
+		try_to_do((fflag = send(sock, request.c_str(), request.size(), 0)) == SOCKET_ERROR);
+
+		if (fflag == SOCKET_ERROR)return SocketStatusCode::send_failed;
+		return SocketStatusCode::msg_sent;
 	}
 };
 
@@ -73,14 +87,16 @@ public:
 	SocketPool() { WSAStartup(MAKEWORD(2, 2), &data); }
 	~SocketPool() { WSACleanup(); }
 
-	int add_socket(string host, int port = 80) {
+	int add_client(string host, int port = 80) {
 		Socket * targ = pool[host] = new Socket;
 		if (targ->status == SocketStatusCode::init_failed)
-			return SocketErrorCode::init_failed;
+			return targ->status;
 
 		targ->status = pool[host]->perform_connect(port);
-		//TODO here
+		if (targ->status == SocketStatusCode::connect_failed)
+			return targ->status;
 	}
+
 	int destroy(string host) {
 		delete pool[host];
 		pool.erase(host);
